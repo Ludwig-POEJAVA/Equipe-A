bb8cad97870c7a9567d42e228c78748f39721fab
Ludwig En Formation (rennes) <ludwig.en.formation+github@gmail.com>
2018-01-11 09:39:21 +0100

    Finalisation de creer_compte
    JS et HTML (css à ajouter dans le fichier commun)

--- vroom/creer_compte.js
+++ vroom/creer_compte.js
@@ -4,53 +4,70 @@
 //fonction appelée par le bouton du formulaire de création de compte
 function check()
 {
-	let usr = document.getElementById('username');
-	let pwd = document.getElementById('password');
+	let usr = document.getElementById('username').value;
+	let pwd = document.getElementById('password').value;
+
 	//1-verifier username est valide (e-mail 1@2(.2)
-	if(!checkUsername(username))
+	if(!checkUsername(usr))
 	{
-		//-> msg erreur 1
-		errorMsg = 'erreur nom utilisateur';
-		dispError(errorMsg);
+		dispStatusMsg("Le nom utilisateur n'est pas de la forme a[.a]@aa.aa", 'nok');
 		return;
 	}
-
 	//2-verifier password >1 caractère
-	if(!checkPassword(password))
+	if(!checkPassword(pwd))
 	{
-		//-> msg erreur 2
-		errorMsg = 'erreur mot de passe trop court';
-		dispError(errorMsg);
+		dispStatusMsg('Le mot de passe est trop court','nok');
 		return;
 	}
 	//3-vérifier que username n'existe pas déja
-	if (checkExistingUser(username)) {
-		//-> msg erreur 3
-		errorMsg = 'erreur utilisateur déjà existant';
-		dispError(errorMsg);
+	if (checkExistingUser(usr))
+	{
+		dispStatusMsg("L'utilisateur éxiste déjà",'nok');
 		return;
 	}
 	//4-écrire en mémoire (locale)
-		adduser(username, password);
-		dispError("c'est fait!!!");
-	//-> msg OK
-	//-> msg erreur 4
+	adduser(usr, pwd);
+	dispStatusMsg("utilisateur enregistré");
 }
-
 
 function checkUsername(username)
 {
-	//marche pas encore
-	//source : https://www.regextester.com/19
-	let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/ ;
-	if(regex.localeCompare(username) == 0 )
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+	//https://regex101.com/ pour vérifier. C'était fun !
+	const regex = /^[a-z0-9]+\.?[a-z0-9]*@[a-z0-9]{2,}\.[a-z0-9]{2,}$/img;
+	/*
+	soit un mail de forme a.b@c.d
+		* a vaut au moins un caractère
+		* . est facultatif
+		* b est facultatif
+		* @ est obligatoire
+		* c vaut au moins 2 caractères
+		* . est obligatoire (pas d'addresses locales, désolé)
+		* d vaut au moins 2 caractères
+
+	le terme caractère signifie : abcdefghijklmnopqrstuvwxyz0123456789
+	les tirets et underscores et autres trucs fancy sont ignorés
+	les sous domaines @sous.domaine.tld sont ignorés
+	les mails mega composés jean.louis.david@ sont ignorés
+
+	les flags sont
+		* i pour ignorer la casse (a == A)
+		* m (uniquement pour faire des tests sur un pavé de texte)
+		* g (idem)
+
+	tests :
+		OK :
+			a@aa.aa
+			a.a@aa.aa
+			bob.razowsky@monsters.inc
+			nom.@bob.fr
+		NOK
+			a@a.a (voir les 2 ci dessous)
+			a@aa.a (tld trop court)
+			a@a.aa (domain trop court)
+			jean.louis.david@aaa.aa (username non géré)
+			bob.razowsky@monsters.inc.disney (sous domaines non géré)
+	*/
+	return(regex.test(username));
 }
 
 function checkPassword(password)
@@ -63,37 +80,41 @@ function checkExistingUser(username)
 	let users = localStorage.getItem("users");
 	if (users == null)
 	{
-		users = {};//<-valider le format par défaut
+		//si la BDD est vide l'utilisateur n'existe pas !
+		return false;
 	}
 
-	for (var i = 0; i < users.length; i++) {
-		if (user[i].name == username) {
+	let listUsers = JSON.parse(users);
+	//pour chaque user, comaprer le nom
+	for (let i = 0; i < listUsers.length; i++)
+	{
+		if (listUsers[i].name == username)
+		{
+			//indiquer un résultat positif dès le premier trouvé
 			return true;
 		}		
 	}
+	//si l'on a parcouru toute la liste et que l'on a rien trouvé
 	return false;
 }
 
-function adduser(username, password) {
-let users = localStorage.getItem("users");
+//enregistre la paire usr:pwd dans la BDD
+function adduser(username, password)
+{
+	let users = localStorage.getItem("users");
 	if (users == null)
 	{
-		users = {};//<-valider le format par défaut
+		users = '[]';
 	}
-	var objetUsers = JSON.parse(users);
-	objetUsers.push({
-		name: username,
-		pwd: password
-	})
-
-  localStorage.setItem("users", JSON.stringify(objetUsers));
-
+	let listUsers = JSON.parse(users);
+	listUsers.push({name: username, pwd: password});
+	localStorage.setItem("users", JSON.stringify(listUsers));
 }
 
-function dispError(message)
+//affiche un message pour l'utilisateur dans la fenêtre
+function dispStatusMsg(message, css_class = 'ok')
 {
-	let msgCtnr = document.getElementById('errorMsg');
-	msgCtnr.innerHTML = message;
+	let ctnr = document.getElementById('statusMsg');
+	ctnr.innerHTML = message;
+	ctnr.className = 'info-' + css_class;
 }
-
-let errorMsg = '';
