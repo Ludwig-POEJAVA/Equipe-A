8acafd33843cffdf3b686c0801ac441fefe390f0
Yorann <yorannb@hotmail.fr>
2018-01-11 15:46:34 +0100

    ouais

--- vroom/scripts/connexion.js
+++ vroom/scripts/connexion.js
@@ -18,6 +18,7 @@ function login()
 	//tout est bon
 	dispStatusMsg("utilisateur connecté");
 	//rediriger
+	setConnectedUser(username);
 	window.location.href = './homepageclient.html';
 	return true;
 }
@@ -88,20 +89,31 @@ function dispStatusMsg(message, css_class = 'ok')
 	ctnr.className = 'info-' + css_class;
 }
 
+function setConnectedUser(username)
+{
+	localStorage.setItem("userConnexion", username);
+}
+
 
 
 
 function deconnexion () {
-	userConnexion = null;
-	passwordConnexion = null;
-	//suppression du bouton après déconnexion => Voir fct connexion
-	document.getElementById("connexion").innerHTML = '<a class="main-nav-link" href="./connexion.htm" title="connexion">Connexion</a>';
-	document.getElementById("boutonDeco").innerHTML = '';
+	if (localStorage.getItem("userConnexion") == null)
+	{
+		userConnexion = null;
+		passwordConnexion = null;
+		//suppression du bouton après déconnexion => Voir fct connexion
+		document.getElementById("connexion").innerHTML = '<a class="main-nav-link" href="./connexion.htm" title="connexion">Connexion</a>';
+		document.getElementById("boutonDeco").innerHTML = '';
+	}
 }
 
 function connexion() {
-	document.getElementById("boutonDeco").innerHTML = '<button onclick="deconnexion()">Déconnexion</button>';
-	document.getElementById("connexion").innerHTML = '';
+	if (localStorage.getItem("userConnexion").length > 1)
+	{
+		document.getElementById("boutonDeco").innerHTML = '<button onclick="deconnexion()">Déconnexion</button>';
+		document.getElementById("connexion").innerHTML = '';
+	}
 }
 
 
